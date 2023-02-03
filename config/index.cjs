a398ceae4832d75b00fb1703e29cba84198cb4f6
zkc199 <97511787+zkc199@users.noreply.github.com>
2023-02-03 09:18:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oavkT5-qiO1Up_mBypXeaVyJCo6A',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'D8T8yC8thFwl91-ubbIg2WrmQMgJVLdByDG2xzek4fs',
+      useTemplateId: 'W1K217Y9nJH8mqEOAwe926wx9e4yCkaXD7QqY1U3bLE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-15',
       festivals: [
@@ -45,7 +45,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'D8T8yC8thFwl91-ubbIg2WrmQMgJVLdByDG2xzek4fs',
+  CALLBACK_TEMPLATE_ID: 'W1K217Y9nJH8mqEOAwe926wx9e4yCkaXD7QqY1U3bLE',
 
   CALLBACK_USERS: [
     {
